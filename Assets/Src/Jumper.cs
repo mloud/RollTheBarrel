1924e010781e2bb10dc4d82593dc95ea9efe5c4b
Louda, Michal <mloud@seznam.cz>
2014-08-15 14:41:08 +0200

    update - jumper added

---
+++ Assets/Src/Jumper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jumper : MonoBehaviour {
+
+	[SerializeField]
+	float force;
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+	
+	}
+
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == "Player")
+		{
+			collider.gameObject.rigidbody.AddForce(transform.up * force, ForceMode.Impulse);
+		}
+	}
+}
