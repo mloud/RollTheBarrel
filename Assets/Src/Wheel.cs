42dcd1c0bcdd5640b8a4ed8c0d84e86f63da7153
Louda, Michal <mloud@seznam.cz>
2014-08-20 13:51:10 +0200

    update - wheel again + parallax layers

---
+++ Assets/Src/Wheel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wheel : MonoBehaviour {
+
+	[SerializeField]
+	float speed;
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update () 
+	{
+		//transform.Rotate(new Vector3(0,0,speed * Time.deltaTime), Space.Self);
+		//rigidbody.AddTorque(new Vector3(0, 0, speed), ForceMode.Force);
+		rigidbody.angularVelocity = new Vector3(0, 0, speed);
+	}
+}
